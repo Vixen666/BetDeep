f9f50bef1af1f263da554a70978ed3b65129f276
johannes.roos <johannes.roos@ulpe-laptop.cchoganas.se>
2018-01-30 10:41:31 +0100

    andra

--- src/dataGathering/JSONScrape.java
+++ src/dataGathering/JSONScrape.java
@@ -1,7 +1,14 @@
 package dataGathering;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -18,6 +25,7 @@ public class JSONScrape  {
 	String content;
 	JSONObject obj;
 	JSONArray arr ;
+	
 	private int gamesPlayed = 0; 
 	private ArrayList<Game> games = new ArrayList<Game>();
 	private ArrayList<Team> teams = new ArrayList<Team>();
@@ -25,8 +33,9 @@ public class JSONScrape  {
 
 
 	public JSONScrape() throws IOException, JSONException {
-		content = new String(Files.readAllBytes(Paths.get("C:\\Users\\Vixen666\\eclipse-workspace\\JSON\\src\\dataGathering\\en.json")));
-		obj = new JSONObject(content)  ;
+		//content = new String(Files.readAllBytes(Paths.get("C:\\Users\\Vixen666\\eclipse-workspace\\JSON\\src\\dataGathering\\en.json")));
+		//obj = new JSONObject(content)  ;
+		obj = readJSONfromURL("https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/en.1.json");
 		arr = obj.getJSONArray("rounds");
 	}
 
@@ -40,6 +49,7 @@ public class JSONScrape  {
 				gamesPlayed++;
 			}		
 			System.out.println("---------------");
+			System.out.println("Runda:    "+i);
 			Collections.sort(teams);
 			for(int k = 0; k < 20; k++) {
 				
@@ -76,6 +86,24 @@ public class JSONScrape  {
 			System.out.println(teams.get(i).toString());
 		}
 	}
+	
+	public static JSONObject readJSONfromURL(String url) throws MalformedURLException, IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try(BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")))) {
+			   String jsonText = readAll(rd);
+			      JSONObject json = new JSONObject(jsonText);
+			      return json;
+		}
+	}
+	
+	 private static String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+		  }
 
 	public static void main(String[] args) throws JSONException, IOException {
 		JSONScrape js = new JSONScrape();
